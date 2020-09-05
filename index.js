6c1452164191ce96a6c80b58160c7c92d56a54e8
Re <refanopradana@gmail.com>
2020-09-06 01:27:13 +0700

    02 Add Middleware Security, Logger, and Parser

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //Babel convert
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
 // const express = require('express')
 const app = express()
 
@@ -10,7 +15,12 @@ const betweenParam = (req, res, next) => {
 }
 
 // noinspection JSCheckFunctionSignatures
-app.use(betweenParam);
+//app.use(betweenParam);
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
+app.use(helmet())
+app.use(morgan('dev'))
 
 // noinspection JSCheckFunctionSignatures
 app.get('/', betweenParam,(req,res) =>
