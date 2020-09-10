61980c5dd916ee007e76e9254db6d9f6eb7a5d86
Re <refanopradana@gmail.com>
2020-09-11 00:05:26 +0700

    01 Join

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,3 @@
-export const join   = (req, res) => res.send("Join")
-export const login  = (req, res) => res.send("Login")
-export const logout = (req, res) => res.send("Logout")
+export const join   = (req, res) => res.render("join", {pageTitle: 'Join'})
+export const login  = (req, res) => res.render("login", {pageTitle: 'Login'})
+export const logout = (req, res) => res.render("logout",  {pageTitle: 'Log out'})
