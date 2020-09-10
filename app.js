e20ead396a9722a6cf32520548063cf6887c007a
Re <refanopradana@gmail.com>
2020-09-10 15:59:42 +0700

    03 Local Variable in PUG

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ import {userRouter} from "./router/userRouter";
 import videoRouter from "./router/videoRouter";
 import globalRouter from "./router/globalRouter";
 import routes from "./routes";
+import {localMiddleWare} from "./middlewares";
 // const express = require('express')
 const app = express();
 //extensen pug of layout inside view
 app.set("view engine", "pug");
 
 // noinspection JSCheckFunctionSignatures
+app.use(helmet())
 app.use(cookieParser())
 app.use(bodyParser.json())         // check what content that send
 app.use(bodyParser.urlencoded({extended : true}))
-app.use(helmet())
 app.use(morgan('dev'))
+
+app.use(localMiddleWare);
 
 // noinspection JSCheckFunctionSignatures
 app.use(routes.home, globalRouter)
