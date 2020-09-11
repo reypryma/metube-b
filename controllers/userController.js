69248659ebf53eac0a14dca943742d4bba35e92d
Re <refanopradana@gmail.com>
2020-09-12 00:12:06 +0700

    01 Post and Get Join, change globalRouter.get and globalRouter.post

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,17 @@
-export const join   = (req, res) => res.render("join", {pageTitle: 'Join'})
+// export const join   = (req, res) => {
+//     res.render("join", {pageTitle: 'Join'})
+// }
+
+//this to get data from join also render join template
+export const postJoin   = (req, res) => {
+    console.log(req.body)
+    res.render("join", {pageTitle: 'Join'})
+}
+
+export const getJoin   = (req, res) => {
+    res.render("join", {pageTitle: 'Join'})
+}
+
 export const login  = (req, res) => res.render("login", {pageTitle: 'Login'})
 export const logout = (req, res) => res.render("logout",  {pageTitle: 'Log out'})
 export const editProfile = (req, res) => res.render("editProfile",  {pageTitle: 'Edit Profile'})
--- router/globalRouter.js
+++ router/globalRouter.js
@@ -1,20 +1,22 @@
 import express from "express";
 import routes from "../routes";
 import {home, videoSearch} from "../controllers/videoController";
-import {join, login, logout} from "../controllers/userController";
+import {getJoin, login, logout, postJoin} from "../controllers/userController";
 
 
 //this will be for handle url
 const globalRouter = express.Router()
+//post(..,postName)
+globalRouter.post(routes.join, postJoin)
+globalRouter.get(routes.join, getJoin)
+
 
 // noinspection JSCheckFunctionSignatures
 globalRouter.get(routes.home, home)
 // noinspection JSCheckFunctionSignatures
 globalRouter.get(routes.search, videoSearch)
-globalRouter.get(routes.join, join)
 globalRouter.get(routes.login, login)
 globalRouter.get(routes.logout, logout)
-
 
 
 export default globalRouter;
