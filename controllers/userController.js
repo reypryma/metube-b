fb57b86db8467def44bf0612ab65a5ed8944c125
Re <refanopradana@gmail.com>
2020-09-12 11:04:31 +0700

    01 Create Post login and variable users isAuthenticated (fake)

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,17 +3,33 @@
 // }
 
 //this to get data from join also render join template
+import routes from "../routes";
+
 export const postJoin   = (req, res) => {
-    console.log(req.body)
-    res.render("join", {pageTitle: 'Join'})
+    //console.log(req.body)
+    const {
+        body: {
+            name, email, password, passwordVerify
+        }
+    }=req;
+    if (password !== passwordVerify){
+        res.status(400)
+        res.render("join", {pageTitle: 'Join'})
+    }else {
+        res.redirect(routes.home)
+    }
 }
 
 export const getJoin   = (req, res) => {
     res.render("join", {pageTitle: 'Join'})
 }
 
-export const login  = (req, res) => res.render("login", {pageTitle: 'Login'})
-export const logout = (req, res) => res.render("logout",  {pageTitle: 'Log out'})
+export const getLogin  = (req, res) => res.render("login", {pageTitle: 'Login'})
+export const postLogin  = (req, res) => {
+    res.redirect(routes.home)
+}
+
+export const logout = (req, res) => res.render({pageTitle: 'Log out'})
 export const editProfile = (req, res) => res.render("editProfile",  {pageTitle: 'Edit Profile'})
 export const userDetail = (req, res) => res.render("userDetail",  {pageTitle: 'Detail User'})
 export const changePassword = (req, res) => res.render("changePassword",  {pageTitle: 'Change Password'})
--- router/globalRouter.js
+++ router/globalRouter.js
@@ -1,21 +1,25 @@
 import express from "express";
 import routes from "../routes";
 import {home, videoSearch} from "../controllers/videoController";
-import {getJoin, login, logout, postJoin} from "../controllers/userController";
+import {getJoin, getLogin, logout, postJoin, postLogin} from "../controllers/userController";
 
 
 //this will be for handle url
 const globalRouter = express.Router()
 //post(..,postName)
+// noinspection JSCheckFunctionSignatures
 globalRouter.post(routes.join, postJoin)
 globalRouter.get(routes.join, getJoin)
+
+globalRouter.get(routes.login, getLogin)
+globalRouter.post(routes.login, postLogin)
+
 
 
 // noinspection JSCheckFunctionSignatures
 globalRouter.get(routes.home, home)
 // noinspection JSCheckFunctionSignatures
 globalRouter.get(routes.search, videoSearch)
-globalRouter.get(routes.login, login)
 globalRouter.get(routes.logout, logout)
 
 
