f86073246e3ee3638af90a7b424bd82eb86a2851
Re <refanopradana@gmail.com>
2020-09-12 13:00:32 +0700

    02 Fake user for test {middleware}

--- middlewares.js
+++ middlewares.js
@@ -4,5 +4,10 @@ export const localMiddleWare = (req, res, next) => {
  res.locals.siteName = 'Metube';
  res.locals.routes = routes;
  //forward middle ware to the next handler, goes to appropiatte router
+ res.locals.user = {
+  isAuthenticated: true,
+  id: 1,
+ };
+
  next();
 };
